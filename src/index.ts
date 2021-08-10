6644243e2f1c4bdb10b3307bc9ee6d92304b5496
Brian <bdesiderio92@gmail.com>
2021-08-10 13:45:53 -0300

    fix old method

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { BigNumberish } from 'ethers'
 //import { ecsign } from 'ethereumjs-util'
 import { attributeToHex, signData, stringToBytes, stripHexPrefix, createKeyPair } from "./utils.js";
 
+const ethutils = require("ethereumjs-util");
 
 const b = async () => {
     const web3 = new Web3();
@@ -49,46 +50,86 @@ const b = async () => {
 
 const example2 = async () => {
     // const contract = new Contract("",)
-    const provider = new providers.JsonRpcProvider("HTTP://127.0.0.1:8545");
-    const ownerAddress = '0x06bB4674A4b08d07186b721378C7e241eD85443b';
-    //const ownerPrivateKey = '0x0936af475d2701538aad321f87e0a51f2b297634653393e8cab7290a674009a5';
+    const provider = new providers.JsonRpcProvider("HTTP://127.0.0.1:7545");
+    const identity = '0x06bB4674A4b08d07186b721378C7e241eD85443b';
     const ownerPrivateKey = '0936af475d2701538aad321f87e0a51f2b297634653393e8cab7290a674009a5';
-    const newOwnerAddress = '0x6d197071d41C77A2779B33304B8cC6Ea41f69918';
-    const newOwnerPrivateKey = '5e697f9196588307b9e818fafca38c33f6592c005bfd190820b1d2cc2d608882';
-    const registry = '0x4EA2D8B4c8B54989fa826bb401f2f01424ee6eA0';
+    // const ownerPrivateKey = '5e697f9196588307b9e818fafca38c33f6592c005bfd190820b1d2cc2d608882';
+    const newOwnerAddress = '0x06bB4674A4b08d07186b721378C7e241eD85443b';
+    const registry = '0x7EEb5772eF87C40255a74C7cC05317C08eA64214';
     const wallet = new Wallet(ownerPrivateKey, provider);
-    const uint8ArrayOwnerPrivateKey = Buffer.from('0936af475d2701538aad321f87e0a51f2b297634653393e8cab7290a674009a5', 'hex')
 
-    
+
     const contract: Contract = ContractFactory.fromSolidity(DidRegistryContract)
-        .attach(registry) 
+        .attach(registry)
         .connect(wallet);
 
+    const nonce2 = await contract.nonce(identity);
 
-   
-        const nonce2 = await contract.nonce( ownerAddress );
-		
-        const sig2 = await signData(
-			ownerAddress,
-			ownerPrivateKey,
-            Buffer.from( "changeOwner" ).toString( "hex" ) +
-			stripHexPrefix( newOwnerAddress ),
-			nonce2.toNumber(),
-			registry
-		);
-		await contract.changeOwnerSigned(
-			ownerAddress,
-			sig2.v,
-			sig2.r,
-			sig2.s,
-			newOwnerAddress,
-			{
-				gasLimit: 600000, gasPrice: 20000000000,
-			}
-		);
+    const sig2 = await signData(
+        identity,
+        ownerPrivateKey,
+        Buffer.from("changeOwner").toString("hex") +
+        stripHexPrefix(newOwnerAddress),
+        nonce2.toNumber(),
+        registry
+    );
 
+    console.log(sig2);
+    // await contract.changeOwnerSigned(
+    // 	identity,
+    // 	sig2.v,
+    // 	sig2.r,
+    // 	sig2.s,
+    // 	newOwnerAddress,
+    // 	{
+    // 		gasLimit: 600000, gasPrice: 20000000000,
+    // 	}
+    // );
+}
+
+const example3 = async () => {
+    const provider = new providers.JsonRpcProvider("HTTP://127.0.0.1:7545");
+    const identity = '0x06bB4674A4b08d07186b721378C7e241eD85443b';
+    const ownerPrivateKey = '0936af475d2701538aad321f87e0a51f2b297634653393e8cab7290a674009a5';
+    // const ownerPrivateKey = '5e697f9196588307b9e818fafca38c33f6592c005bfd190820b1d2cc2d608882';
+    const newOwnerAddress = '0x06bB4674A4b08d07186b721378C7e241eD85443b';
+    const registry = '0x7EEb5772eF87C40255a74C7cC05317C08eA64214';
+    const wallet = new Wallet(ownerPrivateKey, provider);
+
+    const contract: Contract = ContractFactory.fromSolidity(DidRegistryContract)
+        .attach(registry)
+        .connect(wallet);
+
+    const nonce = await provider.getTransactionCount(identity);
+
+    let dataToSign = "19007EEb5772eF87C40255a74C7cC05317C08eA64214000000000000000000000000000000000000000000000000000000000000000106bB4674A4b08d07186b721378C7e241eD85443b6368616e67654f776e657206bB4674A4b08d07186b721378C7e241eD85443b";
+    let paylaodHash = Buffer.from(ethutils.sha3(Buffer.from(dataToSign, "hex")));
+    // utils.keccak256(dataToSign);
+
+    const messageBytes = utils.arrayify(paylaodHash);
+
+    let signature = await wallet.signMessage(messageBytes);
+    const sig2 = ethutils.ecsign(messageBytes, Buffer.from(ownerPrivateKey, 'hex'));
+    const signRSV = {
+		r: `0x${sig2.r.toString( "hex" )}`,
+		s: `0x${sig2.s.toString( "hex" )}`,
+		v: sig2.v
+    };
+
+    let sig = utils.splitSignature(signature);
+
+    console.log("Public Key", wallet.publicKey);
+    console.log("Address", wallet.address);
+
+    // console.log("Recovered:", utils.verifyMessage(utils.arrayify(payloadHash), sig));
+    // console.log("recoverAddress:", utils.recoverAddress(payloadHash, sig));
+    // console.log("recoverPublicKey:", utils.recoverAddress(payloadHash, sig));
+
+    // await contract.changeOwnerSigned(ownerAddress, sig.v, sig.r, sig.s, newOwnerAddress,
+    //     { from: ownerAddress, gasLimit: 600000, gasPrice: 20000000000, nonce: nonce });
 }
 
 
 
-example2();
+example3();
+// example3();
